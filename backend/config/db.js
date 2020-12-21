71b61b61ca61b5361779af008f0c1fdb9902bc10
Effiongoto <effiongoto98@gmail.com>
2020-12-21 09:27:49 +0100

    Implemented mongodb and custom error handling

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+// const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.green.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1); //exit with failure
+  }
+};
+
+// module.exports = connectDB;
+export default connectDB;
---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, //timestamps shown exactly when user was created
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
