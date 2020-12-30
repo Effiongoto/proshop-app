229855ee717323e2dd095a0554badb3c1b99daa1
Effiongoto <effiongoto98@gmail.com>
2020-12-30 17:13:57 +0100

    Added product search functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,11 +20,11 @@ import {
   PRODUCT_CREATE_REVIEW_FAIL,
 } from '../constants/productConstants';
 
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (keyword = '') => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get('/api/products');
+    const { data } = await axios.get(`/api/products?keyword=${keyword}`);
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
@@ -38,7 +38,6 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
-
 export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
