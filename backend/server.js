a4138ad0f19753f0d0ec363b2d400a7d723b54b3
Effiongoto <effiongoto98@gmail.com>
2020-12-30 16:38:38 +0100

    Added product reviews functionality

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import path from 'path';
 import express from 'express';
 import { config } from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -17,6 +18,10 @@ config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // to accept json data in the body
 app.use(express.json());
